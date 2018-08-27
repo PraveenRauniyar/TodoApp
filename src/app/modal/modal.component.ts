5023d4bb6f5faef5c1117bf447f42a762272f370
Praveeng <guptapraveen370@gmail.com>
2018-08-27 23:24:38 +0530

    Praveen | #4 | Added verification for add todo in database then add locally

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {Todo} from "./todo";
-import {DataService} from "../service/data.service";
+import {Todo} from './todo';
+import {DataService} from '../service/data.service';
+import {Headers, Http} from '@angular/http';
 
 
 @Component({
@@ -12,7 +13,7 @@ import {DataService} from "../service/data.service";
 export class ModalComponent implements OnInit {
   allTodoItems: Array<Todo>;
 
-  constructor(private modalService: NgbModal, private dataService: DataService) {
+  constructor(private http: Http, private modalService: NgbModal, private dataService: DataService) {
 
   }
 
@@ -25,24 +26,40 @@ export class ModalComponent implements OnInit {
   }
 
   addTodo(title, description, reminder) {
-    if (title == '' || reminder == '') {
+    if (title === '' || reminder === '') {
       return;
     }
-    let todo = new Todo(title, description, reminder);
-    this.dataService.saveTodo(todo)
-    this.allTodoItems.unshift(todo);
+    const todo = new Todo(title, description, reminder);
+    const httpOptions = {
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    };
+    this.saveTodoInDatabase(todo, httpOptions);
+  }
 
+  saveTodoInDatabase(todo, httpOptions) {
+    this.http.post('http://localhost:8080/addTodo', JSON.stringify(todo), httpOptions)
+      .map(response => response.json())
+      .subscribe((todoId) => {
+          todo.todoId = todoId;
+          this.allTodoItems.unshift(todo);
+        }, err => console.error(err)
+      );
   }
 
   getTodoItems() {
-    this.dataService.getAllTodo().subscribe((todo) => {
-      this.allTodoItems = todo;
-    });
+    this.http.get('http://localhost:8080')
+      .map(response => response.json())
+      .subscribe((todo) => {
+        this.allTodoItems = todo;
+      });
   }
 
   removeTodo(todo: Todo) {
-    let index = this.allTodoItems.indexOf(todo);
-    this.allTodoItems.splice(index, 1);
-    this.dataService.deleteTodo(todo.todoTitle)
+    const index = this.allTodoItems.indexOf(todo);
+    return this.http.delete('http://localhost:8080/delete/' + todo.todoId).subscribe(() => {
+      this.allTodoItems.splice(index, 1);
+    }, err => console.error(err));
   }
 }
