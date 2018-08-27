3047e5252540cfcfe0957d3e8777fdbe8b5417e1
Praveeng <guptapraveen370@gmail.com>
2018-08-27 16:12:50 +0530

    Praveen | #3 | Added todoId property  in todo class

--- src/app/modal/todo.ts
+++ src/app/modal/todo.ts
@@ -2,6 +2,7 @@ export class Todo {
   todoTitle: string;
   description: string;
   reminder: string;
+  todoId: LongRange;
 
   constructor(title,description,reminder){
     this.todoTitle = title;
